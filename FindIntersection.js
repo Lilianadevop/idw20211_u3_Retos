6ce108309a1f013a436d6375445ace7c31ed9530
Liliana Gonzalez <lielgonzalezgu@ittepic.edu.mx>
2021-05-17 20:38:25 -0600

    Sexto Ejercicio

---
+++ FindIntersection.js
@@ -0,0 +1,10 @@
+function FindIntersection(strArr) { 
+
+  var n = strArr[0].split(', ')
+  var m = strArr[1].split(', ')
+  var result = n.filter(x => m.find(n => n === x))
+  return result.length > 0 ? result.join(',') : 'false'
+
+}
+   
+console.log(FindIntersection(readline()));
